8479dd764ed7c6609a0dac339fc7d7d3fdbdc587
Peter O <poccil14@gmail.com>
2013-08-10 14:43:26 -0400

    re-prepare Turtle parser

--- src/com/upokecenter/rdf/RDFTriple.java
+++ src/com/upokecenter/rdf/RDFTriple.java
@@ -1,93 +1,99 @@
-// Written by Peter Occil, 2013. In the public domain.
-// Public domain dedication: http://creativecommons.org/publicdomain/zero/1.0/
+/*
+Written in 2013 by Peter Occil.  
+Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/
+
+If you like this, you should donate to Peter O.
+at: http://upokecenter.com/d/
+*/
 package com.upokecenter.rdf;
 
 public final class RDFTriple {
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((object == null) ? 0 : object.hashCode());
-		result = prime * result
-				+ ((predicate == null) ? 0 : predicate.hashCode());
-		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
-		return result;
-	}
+  private RDFTerm subject, predicate, object;
 
-	@Override
-	public String toString(){
-		return subject.toString()+" "+predicate.toString()+" "+object.toString()+" .";
-	}
+  public RDFTriple(RDFTerm subject, RDFTerm predicate, RDFTerm object) {
+    setSubject(subject);
+    setPredicate(predicate);
+    setObject(object);
+  }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RDFTriple other = (RDFTriple) obj;
-		if (object == null) {
-			if (other.object != null)
-				return false;
-		} else if (!object.equals(other.object))
-			return false;
-		if (predicate == null) {
-			if (other.predicate != null)
-				return false;
-		} else if (!predicate.equals(other.predicate))
-			return false;
-		if (subject == null) {
-			if (other.subject != null)
-				return false;
-		} else if (!subject.equals(other.subject))
-			return false;
-		return true;
-	}
+  public RDFTriple(RDFTriple triple){
+    if(triple==null)throw new NullPointerException("triple");
+    setSubject(triple.subject);
+    setPredicate(triple.predicate);
+    setObject(triple.object);
+  }
 
-	public RDFTriple(RDFTerm subject, RDFTerm predicate, RDFTerm object) {
-		setSubject(subject);
-		setPredicate(predicate);
-		setObject(object);
-	}
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    RDFTriple other = (RDFTriple) obj;
+    if (object == null) {
+      if (other.object != null)
+        return false;
+    } else if (!object.equals(other.object))
+      return false;
+    if (predicate == null) {
+      if (other.predicate != null)
+        return false;
+    } else if (!predicate.equals(other.predicate))
+      return false;
+    if (subject == null) {
+      if (other.subject != null)
+        return false;
+    } else if (!subject.equals(other.subject))
+      return false;
+    return true;
+  }
 
-	public RDFTriple(RDFTriple triple){
-		if(triple==null)throw new NullPointerException("triple");
-		setSubject(triple.subject);
-		setPredicate(triple.predicate);
-		setObject(triple.object);
-	}
+  public RDFTerm getObject() {
+    return object;
+  }
 
-	public RDFTerm getSubject() {
-		return subject;
-	}
+  public RDFTerm getPredicate() {
+    return predicate;
+  }
 
-	private void setSubject(RDFTerm subject) {
-		if((subject)==null)throw new NullPointerException("subject");
-		if(!(subject.getKind()==RDFTerm.IRI ||
-				subject.getKind()==RDFTerm.BLANK))throw new IllegalArgumentException("doesn't satisfy subject.kind==RDFTerm.IRI || subject.kind==RDFTerm.BLANK");
-		this.subject = subject;
-	}
+  public RDFTerm getSubject() {
+    return subject;
+  }
 
-	public RDFTerm getPredicate() {
-		return predicate;
-	}
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((object == null) ? 0 : object.hashCode());
+    result = prime * result
+        + ((predicate == null) ? 0 : predicate.hashCode());
+    result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+    return result;
+  }
 
-	private void setPredicate(RDFTerm predicate) {
-		if((predicate)==null)throw new NullPointerException("predicate");
-		if(!(predicate.getKind()==RDFTerm.IRI))throw new IllegalArgumentException("doesn't satisfy predicate.kind==RDFTerm.IRI");
-		this.predicate = predicate;
-	}
+  private void setObject(RDFTerm object) {
+    if((object)==null)throw new NullPointerException("object");
+    this.object = object;
+  }
 
-	public RDFTerm getObject() {
-		return object;
-	}
+  private void setPredicate(RDFTerm predicate) {
+    if((predicate)==null)throw new NullPointerException("predicate");
+    if(!(predicate.getKind()==RDFTerm.IRI))throw new IllegalArgumentException("doesn't satisfy predicate.kind==RDFTerm.IRI");
+    this.predicate = predicate;
+  }
 
-	private void setObject(RDFTerm object) {
-		if((object)==null)throw new NullPointerException("object");
-		this.object = object;
-	}
+  private void setSubject(RDFTerm subject) {
+    if((subject)==null)throw new NullPointerException("subject");
+    if(!(subject.getKind()==RDFTerm.IRI ||
+        subject.getKind()==RDFTerm.BLANK))throw new IllegalArgumentException("doesn't satisfy subject.kind==RDFTerm.IRI || subject.kind==RDFTerm.BLANK");
+    this.subject = subject;
+  }
 
-	private RDFTerm subject, predicate, object;
+  @Override
+  public String toString(){
+    return subject.toString()+" "+predicate.toString()+" "+object.toString()+" .";
+  }
 }
